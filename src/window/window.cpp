8ac3600155342d446681468655ad0a3a2d454fe6
🦀 adia-dev <abdoudu78130@gmail.com>
2024-10-28 01:33:31 +0100

    ♻️ Create the window and the renderer separatly + ACCELERATED | VSYNC flags

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -13,31 +13,51 @@ wbz::Window wbz::Window::from_config(const WindowConfig &window_config) {
         "Failed to initialize SDL with `SDL_INIT_VIDEO` flags.");
   }
 
-  // TODO: initialize the window and the renderer separately to avoid the
-  // reinterpret_cast
-  int result = SDL_CreateWindowAndRenderer(
-      window_config.width, window_config.height, window_config.flags,
-      (SDL_Window **)&new_window._window,
-      (SDL_Renderer **)&new_window._renderer);
+  new_window._window.reset(SDL_CreateWindow(
+      window_config.title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+      window_config.width, window_config.height, window_config.flags));
 
-  if (result < 0 || new_window.window() == nullptr ||
-      new_window.renderer() == nullptr) {
-    std::cerr << "Failed to create the SDL2 Window and Renderer; Error = "
-              << SDL_GetError() << std::endl;
-
-    throw std::runtime_error("Failed to create the SDL2 Window and Renderer");
+  if (!new_window._window) {
+    std::cerr << "Failed to create the SDL2 Window; Error = " << SDL_GetError()
+              << std::endl;
+    SDL_Quit();
+    throw std::runtime_error("Failed to create the SDL2 Window.");
   }
 
-  SDL_SetWindowTitle(new_window._window.get(), window_config.title);
-  SDL_SetRenderDrawBlendMode(new_window._renderer.get(), SDL_BLENDMODE_BLEND);
+  new_window._renderer.reset(
+      SDL_CreateRenderer(new_window._window.get(), -1,
+                         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+
+  if (!new_window._renderer) {
+    std::cerr << "Failed to create the SDL2 Renderer; Error = "
+              << SDL_GetError() << std::endl;
+    new_window.cleanup();
+    SDL_Quit();
+    throw std::runtime_error("Failed to create the SDL2 Renderer.");
+  }
+
+  if (SDL_SetRenderDrawBlendMode(new_window._renderer.get(),
+                                 SDL_BLENDMODE_BLEND) < 0) {
+    std::cerr << "Failed to set SDL Renderer blend mode; Error = "
+              << SDL_GetError() << std::endl;
+    new_window.cleanup();
+    SDL_Quit();
+    throw std::runtime_error("Failed to set SDL Renderer blend mode.");
+  }
 
   return new_window;
 }
 
 void wbz::Window::cleanup() {
-  SDL_DestroyWindow(_window.get());
-  SDL_DestroyRenderer(_renderer.get());
+  if (_renderer) {
+    SDL_DestroyRenderer(_renderer.get());
+    _renderer = nullptr;
+  }
 
-  _window = nullptr;
-  _renderer = nullptr;
+  if (_window) {
+    SDL_DestroyWindow(_window.get());
+    _window = nullptr;
+  }
+
+  SDL_Quit();
 }
